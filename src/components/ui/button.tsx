d680142fe771ce72e848915d1eefa119f666f595
Ryan Dey <techrpd15113@gmail.com>
2023-12-19 15:30:53 -0600

    shadcn/ui template components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,37 +1,57 @@
-import Link from "next/link"
-import { cva, VariantProps } from "class-variance-authority"
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
 
-interface ButtonProps extends VariantProps<typeof buttonClasses> {
-  children: React.ReactNode
-  href: string
-}
+import { cn } from "@/lib/utils"
 
-const buttonClasses = cva(
-  "rounded-lg inline-flex items-center gap-1 hover:text-shadow hover:shadow-primary-shadow transition-shadow duration-300 ease-out",
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        primary: "bg-primary",
-        secondary: "bg-primaryDark",
-        tertiary: "",
+        default:
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        small: "text-xs px-3 h-7",
-        medium: "text-sm px-4 h-9",
-        large: "text-md px-6 h-12",
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
       },
     },
     defaultVariants: {
-      variant: "primary",
-      size: "medium",
+      variant: "default",
+      size: "default",
     },
   }
 )
 
-export const Button = ({ children, href, variant, size }: ButtonProps) => {
-  return (
-    <Link href={href} className={buttonClasses({ variant, size })}>
-      {children}
-    </Link>
-  )
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
