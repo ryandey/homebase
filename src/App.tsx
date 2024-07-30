34c0c446157f6e843015b61bbc7b0ef28da0fcd5
Ryan Dey <techrpd15113@gmail.com>
2024-07-30 15:14:47 -0500

    Add new components to app and adjust colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import { About } from "@/components/about";
 import { Hero } from "@/components/hero";
 import { Nav } from "@/components/ui/nav";
 import { Experience } from "@/components/experience";
+import { Work } from "@/components/work";
+import WorkCarousel from "@/components/misc/workCarousel";
+import Contact from "@/components/contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <Nav />
-      <main className="bg-background mx-6">
+      <main>
         <Hero />
         <About />
         <Experience />
+        <Work />
+        <WorkCarousel />
+        <Contact />
       </main>
+      <Footer />
     </>
   );
 }
