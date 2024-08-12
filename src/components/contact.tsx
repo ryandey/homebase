52c4fd4249a8c3901582a64e6d19d91e07817d2b
Ryan Dey <techrpd15113@gmail.com>
2024-08-12 15:37:42 -0500

    Add animation to email button

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,17 +3,25 @@ import { Button } from "./ui/button";
 
 export default function Contact() {
   return (
-    <section className="py-10 mx-6" id="contactSection">
+    <section className="py-10 mx-6 my-5" id="contactSection">
       <h2 className="text-3xl mb-3">Contact Me</h2>
-      <p className="text-muted-foreground mb-6">
+      <p className="text-muted-foreground text-xl mb-8">
         Want to learn more about my projects, hire me, or get freelance work
         done?
       </p>
-      <div className="min-h-16">
-        <Button className="rounded-xl text-lg justify-between px-6 h-16 w-full sm:w-auto">
-          Send me an email <SendHorizonal strokeWidth={1.5} className="ml-2" />
-        </Button>
-      </div>
+
+      <Button
+        className="email-btn rounded-xl text-lg justify-between px-6 h-16 w-full group transition-all duration-300 ease-in-out sm:w-auto hover:shadow-blue-950 hover:shadow-xl"
+        asChild
+      >
+        <a href="mailto:hello@ryandey.xyz">
+          Send me an email{" "}
+          <SendHorizonal
+            strokeWidth={2}
+            className="ml-2 transition-all duration-300 ease-in-out group-hover:translate-x-1.5"
+          />
+        </a>
+      </Button>
     </section>
   );
 }
