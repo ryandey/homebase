ba87694caf3af6767ad55c5eab03c048e04c1181
Ryan Dey <techrpd15113@gmail.com>
2024-08-12 15:32:08 -0500

    Swap nav to functional component

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -2,10 +2,10 @@ import { MenuIcon } from "lucide-react";
 import Logo from "@/assets/logo.svg";
 import { Button } from "@/components/ui/button";
 
-export const Nav = () => {
+export default function Nav() {
   return (
     // Mobile Nav
-    <div className="z-50 h-20 px-6 py-2 fixed top-0 w-full backdrop-blur-sm bg-background/50 border-b/75">
+    <div className="z-50 h-20 px-6 py-2 fixed top-0 w-full backdrop-blur-sm bg-background/50 border-b/50">
       <div className="flex items-center justify-between h-full">
         {/* Logo */}
         <a href="/">
@@ -19,4 +19,4 @@ export const Nav = () => {
       </div>
     </div>
   );
-};
+}
