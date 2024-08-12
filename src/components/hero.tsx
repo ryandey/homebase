67e1ce469c74c254ed63f4fc197f16a2faa618df
Ryan Dey <techrpd15113@gmail.com>
2024-08-12 16:16:13 -0500

    Add logo to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,46 +1,60 @@
 import { Linkedin, Github, Instagram } from "lucide-react";
+import Logo from "@/assets/logo.svg";
 
 export const Hero = () => {
   return (
     <>
-      <section className="py-16 mt-16 md:mt-28 lg:mt-32 mx-6 md:mb-5">
-        {/* Name */}
-        <h1 className="text-7xl uppercase mb-5">
-          Ryan
-          <br />
-          Dey
-        </h1>
+      <section className="py-16 mt-16 mx-6 grid sm:grid-cols-2 md:mt-28 lg:mt-32 md:mb-5">
+        {/* Text column */}
+        <div>
+          {/* Name */}
+          <h1 className="text-7xl uppercase mb-5">
+            Ryan
+            <br />
+            Dey
+          </h1>
 
-        {/* Subtitle */}
-        <p className="text-muted-foreground text-xl mb-10">
-          Web Developer & UX Designer
-        </p>
+          {/* Subtitle */}
+          <p className="text-muted-foreground text-xl mb-10">
+            Web Developer & UX Designer
+          </p>
 
-        {/* Socials */}
-        <div className="flex gap-3">
-          <a href="https://github.com/ryandey" target="_blank">
-            <Github
-              color="var(--muted-foreground)"
-              strokeWidth={1.5}
-              className="transition-all ease-in-out duration-300 hover:stroke-white"
-            />
-          </a>
+          {/* Socials */}
+          <span className="flex gap-3">
+            <a href="https://github.com/ryandey" target="_blank">
+              <Github
+                color="var(--muted-foreground)"
+                strokeWidth={1.5}
+                className="transition-all ease-in-out duration-300 hover:stroke-white"
+              />
+            </a>
 
-          <a href="https://www.linkedin.com/in/ryanpdey/" target="_blank">
-            <Linkedin
-              color="var(--muted-foreground)"
-              strokeWidth={1.5}
-              className="transition-all ease-in-out duration-300 hover:stroke-white"
-            />
-          </a>
+            <a href="https://www.linkedin.com/in/ryanpdey/" target="_blank">
+              <Linkedin
+                color="var(--muted-foreground)"
+                strokeWidth={1.5}
+                className="transition-all ease-in-out duration-300 hover:stroke-white"
+              />
+            </a>
 
-          <a href="https://www.instagram.com/ryanpdey/" target="_blank">
-            <Instagram
-              color="var(--muted-foreground)"
-              strokeWidth={1.5}
-              className="transition-all ease-in-out duration-300 hover:stroke-white"
-            />
-          </a>
+            <a href="https://www.instagram.com/ryanpdey/" target="_blank">
+              <Instagram
+                color="var(--muted-foreground)"
+                strokeWidth={1.5}
+                className="transition-all ease-in-out duration-300 hover:stroke-white"
+              />
+            </a>
+          </span>
+        </div>
+
+        {/* Logo column */}
+        <div className="hidden sm:flex justify-end">
+          <img
+            src={Logo}
+            alt="Ryan Dey logo"
+            width={400}
+            className="hidden sm:block sm:max-h-lg"
+          />
         </div>
       </section>
     </>
