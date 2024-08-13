fa2e71386abbb8fc3c620997cab42d529bdae43d
Ryan Dey <techrpd15113@gmail.com>
2024-08-13 12:13:27 -0500

    Swapped hero socials for socials component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 import { Linkedin, Github, Instagram } from "lucide-react";
 import Logo from "@/assets/logo.svg";
+import Socials from "./misc/socials";
 
 export const Hero = () => {
   return (
@@ -19,32 +20,7 @@ export const Hero = () => {
             Web Developer & UX Designer
           </p>
 
-          {/* Socials */}
-          <span className="flex gap-3">
-            <a href="https://github.com/ryandey" target="_blank">
-              <Github
-                color="var(--muted-foreground)"
-                strokeWidth={1.5}
-                className="transition-all ease-in-out duration-300 hover:stroke-white"
-              />
-            </a>
-
-            <a href="https://www.linkedin.com/in/ryanpdey/" target="_blank">
-              <Linkedin
-                color="var(--muted-foreground)"
-                strokeWidth={1.5}
-                className="transition-all ease-in-out duration-300 hover:stroke-white"
-              />
-            </a>
-
-            <a href="https://www.instagram.com/ryanpdey/" target="_blank">
-              <Instagram
-                color="var(--muted-foreground)"
-                strokeWidth={1.5}
-                className="transition-all ease-in-out duration-300 hover:stroke-white"
-              />
-            </a>
-          </span>
+          <Socials />
         </div>
 
         {/* Logo column */}
