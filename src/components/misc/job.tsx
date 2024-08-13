2e8b196589e48e13099d89bff1aeb572b718ab67
Ryan Dey <techrpd15113@gmail.com>
2024-08-13 13:05:56 -0500

    Add tooltips to job company links

--- src/components/misc/job.tsx
+++ src/components/misc/job.tsx
@@ -1,5 +1,11 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, ExternalLink } from "lucide-react";
 import { Badge } from "../ui/badge";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
 
 export type JobModel = {
   company: string;
@@ -20,41 +26,55 @@ export default function Job(props: JobProps) {
     props.job;
 
   return (
-    <div className="py-4">
-      <ul>
-        {/* Dates */}
-        <li className="text-sm text-muted-foreground">
-          {started} &mdash; {ended}
-        </li>
+    <TooltipProvider>
+      <div className="py-4">
+        <ul>
+          {/* Dates */}
+          <li className="text-sm text-muted-foreground">
+            {started} &mdash; {ended}
+          </li>
 
-        {/* Job title and Company */}
-        <li className="font-heading mb-1">
-          <h3 className="capitalize inline">
-            {title} &#183;{" "}
-            <a href={website} target="_blank">
-              {company}
-            </a>
-          </h3>
-        </li>
+          {/* Job title and Company */}
+          <li className="font-heading mb-1">
+            <h3 className="capitalize inline">{title} &#183; </h3>
+            <Tooltip>
+              <TooltipTrigger>
+                <a
+                  href={website}
+                  target="_blank"
+                  className="capitalize transition-all duration-300 hover:text-muted-foreground"
+                >
+                  {company}
+                </a>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-1">
+                <p>
+                  Go to <span className="font-bold">{website}</span>
+                </p>
+                <ExternalLink size={18} />
+              </TooltipContent>
+            </Tooltip>
+          </li>
 
-        {/* Description */}
-        <li className="text-muted-foreground mb-2">{description}</li>
+          {/* Description */}
+          <li className="text-muted-foreground mb-2">{description}</li>
 
-        {/* Tags */}
-        <li>
-          {tags.map((tag) => {
-            return (
-              <Badge
-                key={`tag-${Math.random() * 100}`}
-                variant="outline"
-                className="mr-1"
-              >
-                {tag}
-              </Badge>
-            );
-          })}
-        </li>
-      </ul>
-    </div>
+          {/* Tags */}
+          <li>
+            {tags.map((tag) => {
+              return (
+                <Badge
+                  key={`tag-${Math.random() * 100}`}
+                  variant="outline"
+                  className="mr-1"
+                >
+                  {tag}
+                </Badge>
+              );
+            })}
+          </li>
+        </ul>
+      </div>
+    </TooltipProvider>
   );
 }
