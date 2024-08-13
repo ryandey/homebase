0c5504c9fa49ceeb47c3ca2aad43f27b117783c3
Ryan Dey <techrpd15113@gmail.com>
2024-08-13 15:10:00 -0500

    Add year of project and increase column count

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -5,7 +5,7 @@ export function Work() {
     <section className="mx-6 my-10 py-5" id="workSection">
       <h2 className="text-3xl mb-5">My Work</h2>
 
-      <div className="grid gap-4 md:grid-cols-3 mb-4">
+      <div className="grid gap-4 md:grid-cols-3">
         {PROJECTS.map((project) => {
           return <Project key={project.title} project={project} />;
         })}
@@ -17,16 +17,19 @@ export function Work() {
 const PROJECTS = [
   {
     title: "Radiata Web",
+    year: "2024",
     previewUrl: "https://i.imgur.com/1a2CmuY.jpeg",
     projectUrl: "/projects/radiata-web",
   },
   {
     title: "Ryandey.xyz",
+    year: "2024",
     previewUrl: "https://i.imgur.com/1a2CmuY.jpeg",
     projectUrl: "/projects/radiata-web",
   },
   {
     title: "Jass ATM",
+    year: "2024",
     previewUrl: "https://i.imgur.com/1a2CmuY.jpeg",
     projectUrl: "/projects/radiata-web",
   },
