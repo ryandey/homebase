b3116ad2f632beae8b56443f6e616da6288f9bd8
Ryan Dey <techrpd15113@gmail.com>
2024-08-12 21:42:38 -0500

    Add hamburger menu button and extract mobile nav to separate component

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -1,22 +1,33 @@
-import { MenuIcon } from "lucide-react";
 import Logo from "@/assets/logo.svg";
-import { Button } from "@/components/ui/button";
+import MobileMenu from "./mobile-menu";
+import { useState } from "react";
+import { Twirl as Hamburger } from "hamburger-react";
 
 export default function Nav() {
-  return (
-    // Mobile Nav
-    <div className="z-50 h-20 px-6 py-2 fixed top-0 w-full backdrop-blur-sm bg-background/50 border-b/50">
-      <div className="flex items-center justify-between h-full">
-        {/* Logo */}
-        <a href="/">
-          <img src={Logo} alt="Ryan Dey Logo" className="h-6" />
-        </a>
+  const [isOpen, setIsOpen]: boolean = useState(false);
 
-        {/* Menu Button */}
-        <Button variant="ghost" className="bg-blue px-2">
-          <MenuIcon size={30} color="var(--foreground)" />
-        </Button>
+  return (
+    <>
+      <div className="z-50 h-20 px-6 py-2 fixed top-0 w-full backdrop-blur-sm bg-background/50 border-b/50">
+        <div className="flex items-center justify-between h-full">
+          {/* Logo */}
+          <a href="/">
+            <img src={Logo} alt="Ryan Dey Logo" className="h-6" />
+          </a>
+
+          {/* Menu Button */}
+          <Hamburger
+            toggled={isOpen}
+            size={25}
+            toggle={setIsOpen}
+            color="var(--foreground)"
+            easing="ease-in"
+            duration={0.3}
+            rounded
+          />
+        </div>
       </div>
-    </div>
+      {isOpen && <MobileMenu open={isOpen} />}
+    </>
   );
 }
