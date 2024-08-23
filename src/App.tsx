3a5169f79b236002867f30a2be94aaf445e408f3
Ryan Dey <techrpd15113@gmail.com>
2024-08-23 17:14:53 -0500

    Remove project page route

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,16 @@ import "./App.css";
 import Home from "./components/pages/Home";
 import Projects from "./components/pages/Projects";
 import Nav from "./components/ui/nav";
-import JassATM from "./components/projects/jassatm";
 
 function App() {
   return (
     <>
       <Nav />
-      <main className="mx-auto min-h-screen max-w-screen-lg">
+      <main className="mx-auto mt-32 min-h-screen max-w-screen-lg">
         <Routes>
           {/* Main pages */}
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-
-          {/* Project pages */}
-          <Route path="/projects/jass-atm" element={<JassATM />} />
         </Routes>
       </main>
     </>
