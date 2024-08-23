12a9ba8b55d9e9cc192437d435530831fe1e930d
Ryan Dey <techrpd15113@gmail.com>
2024-08-23 15:43:25 -0500

    Add Not Found page

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center gap-5">
+      <span className="text-center p-4">
+        <h1 className="text-2xl">You aren't supposed to be here...</h1>
+        <p className="text-muted-foreground">
+          Unfortunately, there's nothing to be seen on this page.
+        </p>
+      </span>
+      <Button asChild>
+        <Link to="/">Go back home</Link>
+      </Button>
+    </div>
+  );
+}
+
+export default NotFound;
