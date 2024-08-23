aa1f28bde4ee18ad86ef81b0a2901ba9f915581d
Ryan Dey <techrpd15113@gmail.com>
2024-08-23 17:16:20 -0500

    Add project list to all projects page

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,3 +1,18 @@
+import { ProjectProps } from "../../types";
+import { Project } from "../misc/project";
+import AllProjects from "../../assets/projects.json";
+
 export default function Projects() {
-  return <div className="mt-20">all projects</div>;
+  return (
+    <section className="mt-20 mx-6 my-10 py-5">
+      <h1 className="text-3xl mb-6">My Work</h1>
+
+      <div className="grid gap-4 md:grid-cols-3">
+        {/* Map ALL projects */}
+        {AllProjects.map((project: ProjectProps) => {
+          return <Project key={project.title} project={project} />;
+        })}
+      </div>
+    </section>
+  );
 }
