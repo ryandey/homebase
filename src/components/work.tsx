8e1c866fd27e466e01ffd37a7c348ef48ffe5b0f
Ryan Dey <techrpd15113@gmail.com>
2024-08-24 12:22:23 -0500

    Add "all projects" link in work section

--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,11 +1,29 @@
 import { ProjectProps } from "../types";
 import { Project } from "./misc/project";
 import AllProjects from "../assets/projects.json";
+import { ArrowRightIcon } from "lucide-react";
+import { HashLink } from "react-router-hash-link";
 
 export function Work() {
   return (
     <section className="mx-6 my-10 py-5" id="my-work">
-      <h2 className="text-3xl mb-5">My Work</h2>
+      <div className="flex justify-between items-center mb-5">
+        <h2 className="text-3xl">My Work</h2>
+        <HashLink
+          to="/projects#"
+          className="flex flex-row gap-1 items-center group"
+        >
+          <p className="text-sm text-muted-foreground transition-all ease-in-out duration-300 group-hover:text-white">
+            All Projects
+          </p>
+          <ArrowRightIcon
+            size={18}
+            color="var(--muted-foreground)"
+            strokeWidth={1.5}
+            className="transition-all ease-in-out duration-300 group-hover:stroke-white"
+          />
+        </HashLink>
+      </div>
 
       <div className="grid gap-4 md:grid-cols-3">
         {/* Limit # of projects shown to 3 */}
