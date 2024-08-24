2f3b111ba909fda65f8ea0bfc9650ced596722ae
Ryan Dey <techrpd15113@gmail.com>
2024-08-24 12:58:41 -0500

    Add non-live version of project component

--- src/components/misc/project.tsx
+++ src/components/misc/project.tsx
@@ -7,9 +7,9 @@ interface ProjectModel {
 }
 
 export function Project(props: ProjectModel) {
-  const { title, year, previewUrl, projectUrl } = props.project;
+  const { title, subtitle, year, previewUrl, projectUrl, live } = props.project;
 
-  return (
+  return live ? (
     <a href={projectUrl} target="_blank">
       {/* Card */}
       <motion.div
@@ -21,8 +21,6 @@ export function Project(props: ProjectModel) {
       >
         {/* Card container */}
         <div className="p-4 h-full flex flex-col justify-between">
-          {/* <img src={previewUrl} className="object-cover rounded-lg" /> */}
-
           {/* Topside */}
           <div className="flex flex-row justify-between items-center">
             <p className="text-sm">{year}</p>
@@ -30,9 +28,32 @@ export function Project(props: ProjectModel) {
           </div>
 
           {/* Title */}
-          <p className="work-card-title text-xl">{title}</p>
+          <div>
+            <p className="work-card-title text-xl">{title}</p>
+            <p className="text-muted-foreground">{subtitle}</p>
+          </div>
         </div>
       </motion.div>
     </a>
+  ) : (
+    // Project is not live card
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      className={`min-h-56 h-full rounded-lg transition-shadow duration-300 hover:shadow-xl bg-cover bg-center md:min-h-64 hover:cursor-not-allowed`}
+      style={{
+        backgroundImage: `url(${previewUrl})`,
+      }}
+    >
+      <div className="p-4 h-full flex flex-col justify-between">
+        <div className="flex flex-row justify-between items-center">
+          <p className="text-sm">{year}</p>
+        </div>
+
+        <div>
+          <p className="work-card-title text-xl">{title}</p>
+          <p className="text-muted-foreground">{subtitle}</p>
+        </div>
+      </div>
+    </motion.div>
   );
 }
