ae88acbb4addad3f56f7911fb2dafe557999bcb4
ryandey <techrpd15113@gmail.com>
2024-08-24 22:20:57 -0500

    Add scroll control functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./components/pages/Home";
-import Projects from "./components/pages/Projects";
-import Nav from "./components/ui/nav";
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
+import Home from "./components/pages/Home"
+import Projects from "./components/pages/Projects"
+import Nav from "./components/ui/nav"
+import Footer from "./components/footer"
+import { useEffect } from "react"
+import ScrollToTop from "./lib/scroll"
 
 function App() {
+  useEffect(() => {
+    window.history.scrollRestoration = "manual"
+  }, [])
+
   return (
     <>
       <Nav />
+      <ScrollToTop />
       <Routes>
         {/* Main pages */}
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
