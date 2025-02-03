b24d3b05683ec5dccf6ac5e39c18f8b66abbdd2e
Ryan Dey <ryandeyp@gmail.com>
2025-02-03 17:02:04 -0600

    Update Projects section

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,7 +1,7 @@
-import { ProjectProps } from "../../types"
-import { Project } from "../misc/project"
-import AllProjects from "../../assets/projects.json"
-import Contact from "../contact"
+import { ProjectProps } from "../../types";
+import { Project } from "../misc/project";
+import AllProjects from "../../assets/projects.json";
+import Contact from "../contact";
 
 export default function Projects() {
   return (
@@ -11,17 +11,17 @@ export default function Projects() {
         Projects
       </h1>
       <p className="text-muted-foreground text-xl mb-10">
-        A collection of my personal and freelance web development work
+        A collection of my public development work
       </p>
 
-      <div className="grid gap-4 mb-10 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4">
         {/* Map ALL projects */}
         {AllProjects.map((project: ProjectProps) => {
-          return <Project key={project.title} project={project} />
+          return <Project key={project.title} project={project} />;
         })}
       </div>
 
       <Contact />
     </main>
-  )
+  );
 }
