95d64ae3660904e290c3369239ad1d8e7e5d8713
Ryan Dey <ryandeyp@gmail.com>
2025-02-03 16:58:42 -0600

    Update job component

--- src/components/misc/job.tsx
+++ src/components/misc/job.tsx
@@ -26,55 +26,56 @@ export default function Job(props: JobProps) {
     props.job;
 
   return (
-    <TooltipProvider>
-      <div className="py-4">
+    <div>
+      <TooltipProvider>
         <ul>
-          {/* Dates */}
-          <li className="text-sm text-muted-foreground">
-            {started} &mdash; {ended}
-          </li>
-
           {/* Job title and Company */}
-          <li className="font-heading mb-1">
-            <h3 className="capitalize inline">{title} &#183; </h3>
-            <Tooltip>
-              <TooltipTrigger>
-                <a
-                  href={website}
-                  target="_blank"
-                  className="capitalize transition-all duration-300 hover:text-muted-foreground"
-                >
-                  {company}
-                </a>
-              </TooltipTrigger>
-              <TooltipContent className="flex flex-row gap-1">
-                <p>
-                  Go to <span className="font-bold">{website}</span>
-                </p>
-                <ExternalLink size={18} />
-              </TooltipContent>
-            </Tooltip>
-          </li>
+          <div className="flex flex-col justify-between items-start mb-1 sm:flex-row sm:items-center">
+            <li className="font-heading text-lg space-x-2 sm:text-xl">
+              <h3 className="capitalize inline text-lg sm:text-xl">{title}</h3>
+
+              <p className="font-bold text-lg inline sm:text-xl">&#183;</p>
+
+              <Tooltip>
+                <TooltipTrigger>
+                  <a
+                    href={website}
+                    target="_blank"
+                    className="capitalize transition-all duration-300 hover:text-muted-foreground"
+                  >
+                    {company}
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent className="flex flex-row gap-1">
+                  <p>
+                    Go to <span className="font-bold">{website}</span>
+                  </p>
+                  <ExternalLink size={18} />
+                </TooltipContent>
+              </Tooltip>
+            </li>
+
+            {/* Dates */}
+            <li>
+              {started} &mdash; {ended}
+            </li>
+          </div>
 
           {/* Description */}
           <li className="text-muted-foreground mb-2">{description}</li>
 
           {/* Tags */}
-          <li>
+          <span className="flex gap-2 mt-3 flex-wrap">
             {tags.map((tag) => {
               return (
-                <Badge
-                  key={`tag-${Math.random() * 100}`}
-                  variant="outline"
-                  className="mr-1"
-                >
+                <Badge key={`tag-${Math.random() * 100}`} variant="secondary">
                   {tag}
                 </Badge>
               );
             })}
-          </li>
+          </span>
         </ul>
-      </div>
-    </TooltipProvider>
+      </TooltipProvider>
+    </div>
   );
 }
