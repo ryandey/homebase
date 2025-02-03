d4a7955f7868182b7c0c8faf972e96f67a3a12d0
Ryan Dey <ryandeyp@gmail.com>
2025-02-03 16:57:57 -0600

    Update experience section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,66 +1,87 @@
-import { ArrowRightIcon } from "lucide-react"
-import Job from "../components/misc/job"
+import Job from "../components/misc/job";
 
 export const Experience = () => {
   return (
     <section className="flex flex-col my-10 py-5" id="experience">
-      <h2 className="text-3xl">Experience</h2>
+      <h2 className="text-3xl"></h2>
 
-      {/* Jobs */}
-      <div className="mb-4">
-        {JOB_LIST.map((job) => {
-          return <Job key={job.title} job={job} />
-        })}
+      <div className="flex flex-row justify-between items-center mb-5">
+        <h2 className="text-3xl">Experience</h2>
+
+        <a
+          href="/Ryan_Dey_Resume_RDXYZ_Aug-2024.pdf"
+          target="_blank"
+          className="flex flex-row gap-1 items-center group py-1"
+        >
+          <p className="text-sm text-muted-foreground transition-all ease-in-out duration-300 group-hover:text-white">
+            View full résumé
+          </p>
+        </a>
       </div>
 
-      <a
-        href="/Ryan_Dey_Resume_RDXYZ_Aug-2024.pdf"
-        target="_blank"
-        className="flex flex-row gap-1 items-center group py-1"
-      >
-        <p className="text-sm text-muted-foreground transition-all ease-in-out duration-300 group-hover:text-white">
-          View full résumé
-        </p>
-        <ArrowRightIcon
-          size={18}
-          color="var(--muted-foreground)"
-          strokeWidth={1.5}
-          className="transition-all ease-in-out duration-300 group-hover:stroke-white"
-        />
-      </a>
+      {/* Jobs */}
+      <div className="flex flex-col mb-4 gap-8">
+        {JOB_LIST.map((job) => {
+          return <Job key={job.title} job={job} />;
+        })}
+      </div>
     </section>
-  )
-}
+  );
+};
 
 const JOB_LIST = [
   {
     company: "Florida Blue",
-    title: "Associate IT Developer",
+    title: "Software Developer",
     started: "May 2023",
     ended: "Present",
     description:
-      "Conceptualize and deliver React.js apps for data ingest platforms, including collaboration with project leads and UX engineers for rapid prototyping.",
-    tags: ["JavaScript", "React.js", "Express.js", "PostgreSQL"],
+      "Participate in the research and development of user interface improvements and innovations on cross-functional projects through communication with product owners, leadership, and users. Explore strategies to implement artificial intelligence into legacy applications to improve throughput across the organization.",
+    tags: [
+      "JavaScript",
+      "TypeScript",
+      "React",
+      "Express",
+      "OpenShift",
+      "Jenkins",
+      "Figma",
+    ],
     website: "https://floridablue.com",
   },
   {
     company: "Radiata Web",
-    title: "Freelance Web Developer",
-    started: "Apr. 2023",
+    title: "Software Engineer",
+    started: "April 2023",
     ended: "Present",
     description:
-      "Guide clients through the process of building a bespoke web solution and provide hosting solutions through AWS with automated CI/CD pipelines that support future iterations.",
-    tags: ["TypeScript", "React.js", "Next.js", "PostgreSQL"],
+      "Work with small local businesses and key stakeholders to determine user requirements for a website, then design, prototype, and develop a custom web solution tailored to those needs. Communicate directly with business owners throughout each stage to ensure the delivery of a website that meets every functional, accessible, and aesthetic requirement. ",
+    tags: [
+      "TypeScript",
+      "React",
+      "Next.js",
+      "SSR",
+      "PostgreSQL",
+      "AWS",
+      "Figma",
+    ],
     website: "https://radiataweb.netlify.app/",
   },
   {
     company: "University of North Florida",
-    title: "Webmaster",
-    started: "Feb. 2022",
+    title: "IT Analyst",
+    started: "July 2021",
     ended: "May 2023",
     description:
-      "Migrated 50+ web pages to a modern CMS solution via rebuilding each page with  modern web standards and ADA accessibility. Integrated with an SSO API to serve role-based content to users.",
-    tags: ["Cascade CMS", "HTML/CSS", "REST API"],
+      "Provided graphic design, web development, IT support, and device management services to faculty, University-designated individuals, and individual departments across the University of North Florida. Collaborated with senior graphic designers and web developers to update and design web applications, faculty pages, and event pages to encourage student and faculty involvement and awareness.",
+    tags: [
+      "Python",
+      "HTML/CSS",
+      "Cascade CMS",
+      "SSO",
+      "REST API",
+      "Active Directory",
+      "JAMF",
+    ],
     website: "https://unf.edu",
   },
-]
+];
