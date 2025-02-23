57895eb866c33a6d0ce7eb0a35d657e4958784f7
Ryan Dey <ryandeyp@gmail.com>
2025-02-23 14:34:30 -0600

    Adjust icon button size

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 px-3 has-[>svg]:px-2.5",
         lg: "h-10 px-6 has-[>svg]:px-4",
-        icon: "size-16",
+        icon: "size-14",
       },
     },
     defaultVariants: {
