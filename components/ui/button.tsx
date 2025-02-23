e2282d6c4d5f82382278086ca5eff3d5ef43f08a
Ryan Dey <ryandeyp@gmail.com>
2025-02-23 00:27:14 -0600

    Adjust button dark mode compatibility

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 hover:shadow-pop",
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 hover:shadow-pop dark:text-foreground",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 px-3 has-[>svg]:px-2.5",
         lg: "h-10 px-6 has-[>svg]:px-4",
-        icon: "size-9",
+        icon: "size-16",
       },
     },
     defaultVariants: {
