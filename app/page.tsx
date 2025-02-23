06e068cf81809ff59d5373fd7d66d5eeb9c42751
Ryan Dey <ryandeyp@gmail.com>
2025-02-23 00:28:46 -0600

    Add h2 headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import { Button } from "@/components/ui/button";
-import Hero from "./hero";
-import ThemeToggle from "@/components/theme-toggle";
+import Hero from "../components/landing/hero";
 
 export default function Home() {
   return (
     <div className="h-[100vh]">
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <p>Paragraph</p>
-      <div className="flex flex-row gap-4 mt-4">
-        <Button variant="default">See my work</Button>
-        <Button variant="outline">Get my resume</Button>
-        <Button variant="secondary">Get my resume</Button>
-        <ThemeToggle />
-      </div>
       <Hero />
+      <h2 className="text-6xl">
+        A little bit about <span className="text-primary">who I am</span>
+      </h2>
+      <h2 className="text-6xl">
+        Some of the <span className="text-primary">work I've done</span>
+      </h2>
+      <h2 className="text-6xl">
+        Let's <span className="text-primary">get in touch</span>
+      </h2>
     </div>
   );
 }
