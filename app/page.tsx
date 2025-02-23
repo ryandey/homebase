3cad1941a8023c8717a6c17a6c2ef1e4c8cbdec8
Ryan Dey <ryandeyp@gmail.com>
2025-02-23 14:32:31 -0600

    Move homepage sections into own components

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import Hero from "../components/landing/hero";
+import About from "@/components/landing/about";
+import Hero from "@/components/landing/hero";
+import Projects from "@/components/landing/projects";
+import Contact from "@/components/landing/contact";
 
 export default function Home() {
   return (
-    <div className="h-[100vh]">
+    <main className="mt-14">
       <Hero />
-      <h2 className="text-6xl">
-        A little bit about <span className="text-primary">who I am</span>
-      </h2>
-      <h2 className="text-6xl">
-        Some of the <span className="text-primary">work I've done</span>
-      </h2>
-      <h2 className="text-6xl">
-        Let's <span className="text-primary">get in touch</span>
-      </h2>
-    </div>
+      <About />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
