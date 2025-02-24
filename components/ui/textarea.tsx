2f39c7ae471fdb5296fa790294c154d934f9be36
Ryan Dey <ryandeyp@gmail.com>
2025-02-24 14:14:53 -0600

    Remove shadow from textarea

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,8 +7,8 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "font-instrumentSans border-input placeholder:text-muted-foreground focus-visible:border-primary focus-visible:ring-ring/50 focus-visible:shadow-pop aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive flex field-sizing-content min-h-16 w-full border bg-transparent px-3 py-2 text-lg shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-xl",
-        className,
+        "font-instrumentSans border-input placeholder:text-muted-foreground focus-visible:border-primary focus-visible:ring-ring/50 focus-visible:shadow-pop aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive flex field-sizing-content min-h-16 w-full border bg-transparent px-3 py-2 text-lg transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-xl",
+        className
       )}
       {...props}
     />
