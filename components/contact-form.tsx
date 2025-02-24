134769c62bba4e3ffef8f3ef9ac96badd86c89f5
Ryan Dey <ryandeyp@gmail.com>
2025-02-23 20:00:00 -0600

    Create contact form component

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -14,9 +14,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  fullName: z.string(),
+  email: z.string().email().min(5),
+  phone: z.number().min(10).max(10),
+  message: z.string(),
 });
 
 export default function ContactForm() {
@@ -24,7 +28,7 @@ export default function ContactForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      fullName: "",
     },
   });
 
@@ -36,20 +40,64 @@ export default function ContactForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Full Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Ryan" {...field} />
-              </FormControl>
-              <FormDescription>Description</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="grid md:grid-cols-2 md:gap-8">
+          <div className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-3xl">Full Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ryan" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-3xl">Email Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ryan" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-3xl">Phone</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Ryan" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div>
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-3xl">Message</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Ryan" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
