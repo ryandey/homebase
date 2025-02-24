4a0937910e33bfe76abbe155e53942678d15d6c0
ryandey <techrpd15113@gmail.com>
2025-02-24 12:31:21 -0600

    Update theme toggle to be ghostFlat button type

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -18,13 +18,13 @@ export default function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="border-l-none">
+        <Button variant="ghostFlat" size="icon" className="border-l-none">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="mr-1">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
