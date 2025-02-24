67dc892a6c3563e27fe2dd290b8761169b057709
ryandey <techrpd15113@gmail.com>
2025-02-24 12:03:46 -0600

    Add pop shadow to input focus state

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,9 +9,9 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "font-instrumentSans border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-16 w-full min-w-0 border bg-transparent px-4 py-1 text-lg shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-xl",
-        "focus-visible:border-primary focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "focus-visible:border-primary focus-visible:ring-ring/50 focus-visible:ring-[3px] focus-visible:shadow-pop",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        className,
       )}
       {...props}
     />
